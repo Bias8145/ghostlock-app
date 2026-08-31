385cc153f7ac6fff5d98ea56f895c69174724b29
Khaliq <bias.khaliq@gmail.com>
2026-08-31 20:11:52 +0700

    Update offsets in offsets.h for Android kernel

--- src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
+++ src/kernels/6.1.157-android14-11-gbd23337e42e7-ab14791245/offsets.h
@@ -6,25 +6,15 @@
 OFFSETS_ENTRY(
     "6.1.157-android14-11-gbd23337e42e7-ab14791245",
     STRUCT_OFFSETS_6_1,
-
-    /* Derived from Ghostlock App extraction log */
     .pselect_waiter_shift = 1,
-
-    /* Extracted via nm from vmlinux.elf */
-    .off_init_task            = 0x0201F640,
-    .off_init_cred            = 0x02031AA8,
-    .off_root_task_group      = 0x02208580,
-    .off_selinux_enforcing    = 0x01FCA8C0,
+    .off_init_task        = 0x0201F640,
+    .off_init_cred        = 0x02031AA8,
+    .off_root_task_group  = 0x02208580,
+    .off_selinux_enforcing = 0x0225A420,
     .off_selinux_blob_sizes   = 0x015CEB88,
     .off_security_hook_heads  = 0x015CE478,
-
-    /* Derived from Ghostlock App extraction log */
     .off_slide_nfulnl_logger  = 0x020129D0,
-
-    /* Extracted via nm from vmlinux.elf (sysctl_bootid symbol) */
     .off_slide_boot_id        = 0x0227B498,
-
-    /* Derived from Ghostlock App extraction log */
     .off_slide_loggers_0_1    = 0x02012918
 ),
 
